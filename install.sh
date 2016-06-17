90262e4477f556c6d728b8328d886ee77b84e418
Kenneth Torng <ktorng@gmail.com>
2016-06-17 11:35:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ echo -e "\nSETTING UP SYMBOLIC LINKS..."
 ln -sv $HOME/.vim/vimrc $HOME/.vimrc
 ln -sv $HOME/.vim/gvimrc $HOME/.gvimrc
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
 
